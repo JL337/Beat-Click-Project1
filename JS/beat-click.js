f732b56ea28a9fabc6b2374e2b3ddc74ba62829b
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2017-12-18 14:26:17 +0000

    Circles can repeat their clicks, but loop is stuck

--- JS/beat-click.js
+++ JS/beat-click.js
@@ -10,8 +10,23 @@
 
 $(document).ready(function () {
 
+// var randomChoice = Math.floor((Math.random() * 3)+1); 
+// console.log("RANDOM CHOICE IS: "+randomChoice);
 console.log( "Javascript Ready To Go!" ); 
 console.log( "START" );
+
+
+var currentSize = 450;
+var minSize = 200;
+var decreasing = true;
+var checkStatus = 0;
+var button1 = $(".button1");
+var button2 = $(".button2");
+var button3 = $(".button3");
+var circle1 = $(".circle1");
+var circle2 = $(".circle2");
+var circle3 = $(".circle3");
+var randomChoice = 0;
 
 function clickToHome(){
     home.addEventListener("click", function() {
@@ -42,36 +57,11 @@ audioElement.addEventListener("timeupdate", function() {
 
         timeRemain.innerHTML = m+":"+s; //append minutes and seconds left to targeted html id
 
-        if ( m === 0 || s === 0){
-        window.location.href = 'form2.html';
-        }
-    
+        // if ( m === 0 || s === 0){
+        // window.location.href = 'form2.html';
+        // }   
 });
 
-  var currentSize = 450;
-  var minSize = 200;
-  var decreasing = true;
-  var checkStatus = 0;
-  var button1 = $(".button1");
-  var button2 = $(".button2");
-  var button3 = $(".button3");
-  var circle1 = $(".circle1");
-  var circle2 = $(".circle2");
-  var circle3 = $(".circle3");
-  var randomChoice = 0;
-  // var secs = 1000;
-
-// Timing function needs fixing
-//   var id = setInterval(function(){ 
-//   for ( i = 1000 ; i > 0;i --){
-//     secs--; 
-//     console.log(secs);
-//     if(secs = 0){
-//     clearInterval(id);
-//     alert('Game Over!');
-//    }
-//   }
-// }, 1200);
 
 function clickOne(){
   $(".button1")
@@ -82,72 +72,63 @@ function clickOne(){
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', 'Audio/clap.wav');
     audioElement.play();
-    $(this).css("background-color","black");
+    $(this).css("background-color","green");
     //CHECK HERE FOR COMPARISON SCORE
     console.log("clickOne");
   });
 }
 clickOne();
 
-// function clickTwo(){
-//   $(".button2")
-//     .mouseup(function() {
-//     $(this).css("background-color","transparent");
-//   })
-//     .mousedown(function() {
-//     var audioElement = document.createElement('audio');
-//     audioElement.setAttribute('src', 'Audio/whistle.wav');
-//     audioElement.play();
-//     $(this).css("background-color","yellow");
-//     //CHECK HERE FOR COMPARISON SCORE
-//     console.log("clickTwo");
-//   });
-// }
-// clickTwo();
+function clickTwo(){
+  $(".button2")
+    .mouseup(function() {
+    $(this).css("background-color","transparent");
+  })
+    .mousedown(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'Audio/whistle.wav');
+    audioElement.play();
+    $(this).css("background-color","yellow");
+    //CHECK HERE FOR COMPARISON SCORE
+    console.log("clickTwo");
+  });
+}
+clickTwo();
 
-// function clickThree(){
-//   $(".button3")
-//     .mouseup(function() {
-//     $(this).css("background-color","transparent");
-//   })
-//     .mousedown(function() {
-//     var audioElement = document.createElement('audio');
-//     audioElement.setAttribute('src', 'Audio/normal.wav');
-//     audioElement.play();
-//     $(this).css("background-color","red");
-//     //CHECK HERE FOR COMPARISON SCORE
-//     console.log("clickThree");
-//   });
-// }
-// clickThree();
+function clickThree(){
+  $(".button3")
+    .mouseup(function() {
+    $(this).css("background-color","transparent");
+  })
+    .mousedown(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'Audio/normal.wav');
+    audioElement.play();
+    $(this).css("background-color","red");
+    console.log("clickThree");
+  });
+}
+clickThree();
 
 function oneBeat(){
     var randomChoice = Math.floor((Math.random() * 1)+1);   
-    console.log("RANDOM CHOICE: CIRCLE "+randomChoice)
-    // THIS IS BUGGED: Change inner number next to the *, to 1,2 or 3, to use multile circles.
+    console.log("RANDOM CHOICE: CIRCLE "+randomChoice);
+    // Change INNER number next to the *, to 1,2 or 3, to use multile circles.
     // CHANGE OUTER NUMBER ONLY: To ISOLATE single circles, use 1,2,3 respectively.
     if (randomChoice === 1){
-      //var interval1 = setInterval(retractCircle1, 11.32);
       var interval1 = setInterval(function(){ retractCircle1() }, 11.3207547169811);
       console.log("CHOICE === 1");
-      // retractCircle1();
     }
     else if (randomChoice === 2){
       var interval2 = setInterval(function(){ retractCircle2() }, 11.3207547169811);
-      //var interval = setInterval(retractCircle2, 11.32);
       console.log("CHOICE === 2");
-      // retractCircle2();
     }
     else {
       var interval3 = setInterval(function(){ retractCircle3() }, 11.3207547169811);
-      //var interval = setInterval(retractCircle3, 11.32);
       console.log("CHOICE === 3");
-      // retractCircle3();
     }
 }
 oneBeat();
-
-// clearInterval(interval1);
 
   // function checkStatus(){
   //   if (currentSize <= 450  && currentSize >= 370 ) {
@@ -173,10 +154,8 @@ oneBeat();
   // }
 
   function retractCircle1() {
-    //put automatic audio on here, comment two lines back in
-    // var audioElement = document.createElement('audio');
-    // audioElement.setAttribute('src', 'Audio/clap.wav');
-
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'Audio/clap.wav');
     //CHECK what state the outer circle is at
     // if (currentSize <= 450  && currentSize >= 370 ) {
     //   $(".button1").css("background-color","red");
@@ -190,40 +169,21 @@ oneBeat();
     // else{
     //   $(".button1").css("background-color","black");
     // }
-
-    if ((currentSize === minSize) ) {
+    if (currentSize === minSize) {
+      audioElement.play();
       currentSize = 450;
       decreasing = true;
-      //audioElement.play();
-      console.log("CIRCLE 1 CLAP");
-      return;
-      // console.log("currentSize is: "+currentSize);
-      // return currentSize;
+      console.log("CIRCLE 2 CLAP");
     }
     if (decreasing == true) {
       (currentSize = currentSize - 5);
     }
-
     $(".circle1").css("height", currentSize);
-    $(".circle1").css("width", currentSize);
+    $(".circle1").css("width", currentSize);  
+    return;
   }
 
   function retractCircle2() {
-
-    // //CHECK what state the outer circle is at
-    // if (currentSize <= 450  && currentSize >= 370 ) {
-    //   $(".circle1").css("background-color","red");
-    // } 
-    // else if (currentSize <= 369  && currentSize >= 300) {
-    //   $(".circle1").css("background-color","yellow");
-    // } 
-    // else if (currentSize <= 299  && currentSize >= 200) {
-    //   $(".circle1").css("background-color","green");
-    // } 
-    // else{
-    //   $(".circle1").css("background-color","black");
-    // }
-
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', 'Audio/whistle.wav');
     if (currentSize === minSize) {
@@ -231,45 +191,31 @@ oneBeat();
       currentSize = 450;
       decreasing = true;
       console.log("CIRCLE 2 CLAP");
-      return;
     }
     if (decreasing == true) {
       (currentSize = currentSize - 5);
     }
     $(".circle2").css("height", currentSize);
     $(".circle2").css("width", currentSize);  
+    return;
   }
 
+
   function retractCircle3() {
-
-    // //CHECK what state the outer circle is at
-    // if (currentSize <= 450  && currentSize >= 370 ) {
-    //   $(".circle1").css("background-color","red");
-    // } 
-    // else if (currentSize <= 369  && currentSize >= 300) {
-    //   $(".circle1").css("background-color","yellow");
-    // } 
-    // else if (currentSize <= 299  && currentSize >= 200) {
-    //   $(".circle1").css("background-color","green");
-    // } 
-    // else{
-    //   $(".circle1").css("background-color","black");
-    // }
-
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', 'Audio/normal.wav');
     if (currentSize === minSize) {
+      audioElement.play();
       currentSize = 450;
       decreasing = true;
-      audioElement.play();
-      console.log("CIRCLE 3 DRUM");
-      return;
+      console.log("CIRCLE 2 CLAP");
     }
     if (decreasing == true) {
       (currentSize = currentSize - 5);
     }
     $(".circle3").css("height", currentSize);
-    $(".circle3").css("width", currentSize);
+    $(".circle3").css("width", currentSize);  
+    return;
   }
 
 }, 1600); //DELAY ALL WHEN LOAD PLAY-PAGE, DELAY 1.5 seconds.
